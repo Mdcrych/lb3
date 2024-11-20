8f9d13ff3825270e2ab879a31f2c9063ba2fb6db
Mediocrych <148034597+Mdcrych@users.noreply.github.com>
2024-11-20 21:12:28 +0500

    Update lab3_all.cpp

--- lab3_all.cpp
+++ lab3_all.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
+#include <sstream>
 
 class Name {
   private:
@@ -25,6 +26,7 @@ class Name {
     }
     return result;}
 };
+
 class House{
     private:
     int p_floors;
@@ -50,33 +52,133 @@ class House{
         this->p_floors = n_floors;
     }
 };
-class Employee{
-    private:
+class Department;
+
+class Employee {
+private:
     std::string name;
-    std::string department;
-    Employee* boss;
-    public:
-    Employee(std::string name, std::string department) : name(name), department(department), boss(nullptr) {}
-    
-    void setBoss(Employee* boss){
-    this->boss = boss;
+    Department* department;
+public:
+    Employee(std::string n): name(n), department(nullptr) {}
+
+    std::string getName() {
+        return name;
+    }
+
+    void setDepartment(Department* dep) {
+        department = dep;
+    }
+
+    Department* getDepartment() {
+        return department;
     }
 
     std::string toString() {
-        if (boss!= nullptr && boss->department == department) {
-            return name + " является начальником отдела " + department;
-        }
-        else {
-            if(boss != nullptr && boss->name != name){
-                return name + " Работает в отделе " + department + ", начальник которого: " + boss->name;
-            }
-            else{
-                return name + " Работает в отделе " + department + ", у которого нет начальника ";
-            }
-        }
+        return getName();
     }
 };
 
+class Department {
+private:
+    std::string name;
+    Employee* manager;
+    std::vector<Employee*> employees;
+public:
+    Department(std::string n, Employee* m): name(n), manager(m) {}
+
+    std::string getName() {
+        return name;
+    }
+
+    void addEmployee(Employee* emp) {
+        emp->setDepartment(this);
+        employees.push_back(emp);
+    }
+
+    Employee* getManager() {
+        return manager;
+    }
+
+    std::vector<Employee*> getEmployees() {
+        return employees;
+    }
+};
+void printEmployeeInfo(Employee* emp) {
+    if (emp->getDepartment()->getManager() == emp) {
+    std::cout << emp->getName() << " является начальником отдела " << emp->getDepartment()->getName() << std::endl;
+        } 
+    else {
+        std::cout << emp->getName() << " работает в отделе " << emp->getDepartment()->getName() << ", начальник которого " << emp->getDepartment()->getManager()->getName() << std::endl;
+        }
+}
+void printAllEmployeesInDepartment(Employee* emp) {
+    Department* dep = emp->getDepartment();
+    std::vector<Employee*> employees = dep->getEmployees();
+    
+    std::cout << "Сотрудники отдела " << dep->getName() << ": ";
+    for (Employee* employee : employees) {
+        std::cout << employee->getName() << ", ";
+    }
+    std::cout << std::endl;
+}
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Fraction(int num, int denom) : numerator(num), denominator(denom) {}
+
+    std::string getString() {
+        std::stringstream ss;
+        ss << numerator << "/" << denominator;
+        return ss.str();
+    }
+
+    Fraction add(const Fraction& other) {
+        int newNum = numerator * other.denominator + other.numerator * denominator;
+        int newDenom = denominator * other.denominator;
+        return Fraction(newNum, newDenom);
+    }
+
+    Fraction subtract(const Fraction& other) {
+        int newNum = numerator * other.denominator - other.numerator * denominator;
+        int newDenom = denominator * other.denominator;
+        return Fraction(newNum, newDenom);
+    }
+
+    Fraction multiply(const Fraction& other) {
+        int newNum = numerator * other.numerator;
+        int newDenom = denominator * other.denominator;
+        return Fraction(newNum, newDenom);
+    }
+
+    Fraction divide(const Fraction& other) {
+        int newNum = numerator * other.denominator;
+        int newDenom = denominator * other.numerator;
+        return Fraction(newNum, newDenom);
+    }
+
+    Fraction add(int value) {
+        int newNum = numerator + value * denominator;
+        return Fraction(newNum, denominator);
+    }
+
+    Fraction subtract(int value) {
+        int newNum = numerator - value * denominator;
+        return Fraction(newNum, denominator);
+    }
+
+    Fraction multiply(int value) {
+        int newNum = numerator * value;
+        return Fraction(newNum, denominator);
+    }
+
+    Fraction divide(int value) {
+        int newDenom = denominator * value;
+        return Fraction(numerator, newDenom);
+    }
+};
 int main() {
     setlocale(LC_ALL, "Russian");
     int choice = 1;
@@ -124,33 +226,56 @@ int main() {
 
         case 3:
         {
-            Employee Petrov("Петров", "IT");
-            Employee Kozlov("Козлов", "IT");
-            Employee Sidorov("Сидоров", "IT");
-            Kozlov.setBoss(&Kozlov);
-            std::cout << Petrov.toString() << std::endl;
-            std::cout << Kozlov.toString() << std::endl;
-            std::cout << Sidorov.toString() << std::endl;
-            return 0;
+            Employee Petrov("Петров");
+            Employee Kozlov("Козлов");
+            Employee Sidorov("Сидоров");
+            Department IT("IT", &Kozlov);
+            IT.addEmployee(&Kozlov);
+            IT.addEmployee(&Petrov);
+            IT.addEmployee(&Sidorov);
+            printEmployeeInfo(&Petrov);
+            printEmployeeInfo(&Kozlov);
+            printEmployeeInfo(&Sidorov);
         }
             break;
 
         case 6:
             {
-            
+            Employee Petrov("Петров");
+            Employee Kozlov("Козлов");
+            Employee Sidorov("Сидоров");
+            Department IT("IT", &Kozlov);
+            IT.addEmployee(&Kozlov);
+            IT.addEmployee(&Petrov);
+            IT.addEmployee(&Sidorov);
+            printAllEmployeesInDepartment(&Kozlov);
             }
 
         case 4:
-        {
-            
-
-        }
+            {
+            Name name1("", "Клеопатра", "" ); 
+            Name name2("Пушкин", "Александр", "Сергеевич");
+            Name name3("Маяковский", "Владимир", "");
+            Name name4("Христофор", "Бонифатьевич", "" );
+            std::cout << "Имя 1: " << name1.toString() << std::endl;
+            std::cout << "Имя 2: " << name2.toString() << std::endl;
+            std::cout << "Имя 3: " << name3.toString() << std::endl;
+            std::cout << "Имя 4: " << name4.toString() << std::endl;
+            }
             break;
 
         case 5:
-        {
-            
-        }
+            {
+            Fraction f1(1, 3);
+            Fraction f2(2, 3);
+            Fraction f3(3, 4);
+            std::cout << f1.getString() << " + " << f2.getString() << " = " << f1.add(f2).getString() << std::endl;
+            std::cout << f1.getString() << " - " << f2.getString() << " = " << f1.subtract(f2).getString() << std::endl;
+            std::cout << f1.getString() << " * " << f2.getString() << " = " << f1.multiply(f2).getString() << std::endl;
+            std::cout << f1.getString() << " / " << f2.getString() << " = " << f1.divide(f2).getString() << std::endl;
+            Fraction result = f1.add(f2).divide(f3).subtract(5);
+            std::cout << "((" << f1.getString() << " + " << f2.getString() << ") / " << f3.getString() << ") - 5 = " << result.getString() << std::endl;
+            }
             break;
     }
     return 0;
